593d08cea0a3753cb670f630480cf82b145b20dc
pyomelch <omelch.py@gmail.com>
2023-08-29 14:30:26 +0300

    Added endpoint bus_detail with @api_view "GET"&"PUT"&"DELETE"

--- station/views.py
+++ station/views.py
@@ -21,3 +21,26 @@ def bus_list(request):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(["GET", "PUT", "DELETE"])
+def bus_detail(request, pk):
+    try:
+        bus = Bus.objects.get(pk=pk)
+    except Bus.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = BusSerializer(bus)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = BusSerializer(bus, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        bus.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
