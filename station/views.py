88b7058237db25cd238164a74c58adc5d5289f1a
pyomelch <omelch.py@gmail.com>
2023-08-29 14:13:11 +0300

    Added @api_view with "GET"&"POST

--- station/views.py
+++ station/views.py
@@ -1,14 +1,23 @@
-from django.http import JsonResponse
-from django.shortcuts import render
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from station.models import Bus
 from station.serializers import BusSerializer
 
 
+@api_view(["GET", "POST"])
 def bus_list(request):
     if request.method == "GET":
         buses = Bus.objects.all()
         serializer = BusSerializer(buses, many=True)
-        return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == "POST":
+        serializer = BusSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
