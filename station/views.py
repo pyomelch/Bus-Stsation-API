f2460ca6944d7ff416ff090759506a108162ec41
pyomelch <omelch.py@gmail.com>
2023-08-29 14:05:57 +0300

    Created endpoint bus_list

--- station/views.py
+++ station/views.py
@@ -1,3 +1,14 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework import status
 
-# Create your views here.
+from station.models import Bus
+from station.serializers import BusSerializer
+
+
+def bus_list(request):
+    if request.method == "GET":
+        buses = Bus.objects.all()
+        serializer = BusSerializer(buses, many=True)
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
+
