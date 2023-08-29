f4e9b2670f521c4cf726b12f5273bb61aea1b203
pyomelch <omelch.py@gmail.com>
2023-08-29 16:19:50 +0300

    Added generics.APIView to BusList & BusDetail

--- station/views.py
+++ station/views.py
@@ -1,51 +1,38 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, mixins
 
 from station.models import Bus
 from station.serializers import BusSerializer
 
 
-class BusList(APIView):
-    def get(self, request):
-        buses = Bus.objects.all()
-        serializer = BusSerializer(buses, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class BusList(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+):
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer
 
-    def post(self, request):
-        serializer = BusSerializer(data=request.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class BusDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Bus.objects.get(pk=pk)
-        except Bus.DoesNotExist:
-            raise Http404
+class BusDetail(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView
+):
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer
 
-    def get(self, request, pk):
-        bus = self.get_object(pk)
-        serializer = BusSerializer(bus)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk):
-        bus = self.get_object(pk)
-        serializer = BusSerializer(bus, data=request.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        bus = self.get_object(pk)
-        bus.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
